b8814ee93881a6102c076947eaae3cc13e023d36
Peter Njiru <peternjirum@gmail.com>
2023-04-24 16:43:48 -0400

    completed building simple liker lab

--- main.js
+++ main.js
@@ -5,15 +5,55 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+let errorMsg = document.querySelector("#modal")
+errorMsg.className = "hidden"
+
+const liker = document.getElementsByClassName("like-glyph")
+
+for (let element of liker) {
+
+  element.addEventListener("click", function () {
+    // call the fetchUserData function when the button is clicked
+    mimicServerCall().then(userData => {
+
+
+      if (element.textContent == EMPTY_HEART) {
+        element.textContent = FULL_HEART
+        element.className = "activated-heart"
+        element.style.cursor = "pointer"
+
+      } else {
+        element.textContent = EMPTY_HEART
+        element.className = "like-glyph"
+      }
+
+
+
+    }).catch(error => {
+      // handle errors here
+      
+        errorMsg.className = ""
+
+        setTimeout(() => {
+          errorMsg.className = "hidden"
+        }, 3000);
+  
+    });
+  });
+
+}
+
+
+
 
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
